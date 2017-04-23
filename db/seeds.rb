b3b02d249d7526d7e0d1801fda922a013ff77191
mattcritelli <matthew.critelli@gmail.com>
2017-04-22 18:42:03 -0600

    Add Event and User associations.  Update seed data for all 3 models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,29 @@
 
 
 5.times do
-    Event.create(category: Faker::Team.sport.capitalize,
-                 name: Faker::Team.name.upcase + " vs. " + Faker::Team.name.upcase,
-                 date_and_time: Faker::Time.forward(60, :evening),
-                 capacity: 100)
+  Event.create(category: Faker::Team.sport.capitalize,
+                name: Faker::Team.name.upcase + " vs. " + Faker::Team.name.upcase,
+                date_and_time: Faker::Time.forward(60, :evening),
+                capacity: 100)
+end
+
+User.create(admin: true,
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            email: "admin@email.com",
+            password_digest: "password")
+
+2.times do
+  User.create(admin: false,
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              email: Faker::Internet.email,
+              password_digest: "password")
+end
+
+50.times do
+  Ticket.create(event_id: (1..5).to_a.sample,
+                user_id: [2,3].sample,
+                price: [10,20].sample,
+                seat: (1..100).to_a.sample)
 end
