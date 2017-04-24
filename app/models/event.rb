872f4c0d4b47a0c593d14872a831a4879b4fb03a
mattcritelli <matthew.critelli@gmail.com>
2017-04-24 15:45:59 -0600

    Add presence validations for all attributes.  Validates capacity is greater than 0

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   has_many :tickets
   has_many :users, through: :tickets
+  validates_presence_of :category, :name, :date_and_time, :capacity
+  validates_numericality_of :capacity, :greater_than => 0
 
   def format_date
     self.date_and_time.strftime("%b %d, %Y")
