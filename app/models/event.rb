5c023fa0f75c1021fc85938e437e34cb24560e68
mattcritelli <matthew.critelli@gmail.com>
2017-04-24 12:20:35 -0600

    Create initial view for events index page. Add instance methods to event models to format date & time and calculate remaining tickets for each event.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,17 @@
 class Event < ApplicationRecord
   has_many :tickets
   has_many :users, through: :tickets
+
+  def format_date
+    self.date_and_time.strftime("%b %d %Y")
+  end
+
+  def format_time
+    self.date_and_time.strftime("%I %M %p")
+  end
+
+  def tickets_remaining
+    self.capacity - self.tickets.count
+  end
+
 end
