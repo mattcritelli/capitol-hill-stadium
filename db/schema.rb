9b32c81b11c17a88e8e74a5236c2a5d9e91f12a6
mattcritelli <matthew.critelli@gmail.com>
2017-05-03 12:39:24 -0600

    Add user reference to ticket migration/schema.  Update ticket model to separate belongs_to to solve rails arity error.  Update formatting on events index page to center content. Need to test User and Ticket relationship to allow people to ensure correct association.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426192528) do
+ActiveRecord::Schema.define(version: 20170503182031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,9 @@ ActiveRecord::Schema.define(version: 20170426192528) do
     t.string   "seat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["event_id"], name: "index_tickets_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_tickets_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema.define(version: 20170426192528) do
   end
 
   add_foreign_key "tickets", "events"
+  add_foreign_key "tickets", "users"
 end
